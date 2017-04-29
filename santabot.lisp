20d5542fd8d0eded7bf739c1d9d9694031aa0c9c
Alex Recker <alex@reckerfamily.com>
2017-04-29 14:28:37 -0500

    Work

---
+++ santabot.lisp
@@ -0,0 +1,42 @@
+(setf *participants* '(adam eve cain abel))
+
+(defun read-config (path)
+  (with-open-file (s path :direction :INPUT)
+    (let ((json:*json-symbols-package* nil))
+      (json:decode-json s))))
+
+(setf *config* (read-config "~/git/santabot/example-config.json"))
+
+(defun shift (l)
+  (append (last l)
+	  (reverse (cdr (reverse l)))))
+
+(defun match (l)
+  (mapcar #'list l (shift l)))
+
+(defun permute (l)
+  (if (null l) '(())
+      (mapcan #'(lambda (x)
+		  (mapcar #'(lambda (y) (cons x y))
+			  (permute (remove x l :count 1)))) l)))
+
+(defun participants (config)
+  (mapcar #'car (cdr (assoc 'participants config))))
+
+(defun exclusions (name config)
+  (mapcar (lambda (n) (intern (string-upcase n)))
+	  (cdr (assoc 'exclusions
+		      (cdr (assoc name
+				  (cdr (assoc 'participants config))))))))
+
+(defun allowed-match-p (match config)
+  (let ((gifter (first match))
+	(recipient (first (reverse match))))
+    (= 0 (length (member recipient (exclusions gifter config))))))
+
+(defun allowed-possibility-p (matches config)
+  (every (lambda (m) (allowed-match-p m config)) matches))
+
+(defun possibilities (config)
+  (remove-if-not (lambda (p) (allowed-possibility-p p config))
+		 (mapcar #'match (permute (participants config)))))
