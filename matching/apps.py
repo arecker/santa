53f7847c57c8a3e91c5f8f0b235141e21278471e
Alex Recker <arecker@zendesk.com>
2017-12-25 17:19:35 -0600

    i dont know what im doing with my life

--- matching/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MatchingConfig(AppConfig):
-    name = 'matching'
-    
-    def ready(self):
-        import .signals
--- santabot/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-
-
-@method_decorator(login_required, name='dispatch')
-class Home(TemplateView):
-    template_name = "home.html"
